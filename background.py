d0d37dbfdd115fd7bf40176e1a239112739d686b
Andrew Lilja <andrewlilja@gmail.com>
2015-10-26 16:10:54 -0500

    hand color hist applied

--- background.py
+++ background.py
@@ -1,27 +1,106 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
 
-fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False, history=100000)
 
-for i in range(10):
-    init_frame = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY)
+class HandDetection:
+    def __init__(self):
+        self.trained_hand = False
+        self.hand_row_nw = None
+        self.hand_row_se = None
+        self.hand_col_nw = None
+        self.hand_col_se = None
+        self.hand_hist = None
+
+    def draw_hand_rect(self, frame):
+        rows, cols, _ = frame.shape
+
+        self.hand_row_nw = np.array([
+            6 * rows / 20,
+            6 * rows / 20,
+            6 * rows / 20,
+            10 * rows / 20,
+            10 * rows / 20,
+            10 * rows / 20,
+            14 * rows / 20,
+            14 * rows / 20,
+            14 * rows / 20
+        ])
+        self.hand_col_nw = np.array([
+            9 * cols / 20,
+            10 * cols / 20,
+            11 * cols / 20,
+            9 * cols / 20,
+            10 * cols / 20,
+            11 * cols / 20,
+            9 * cols / 20,
+            10 * cols / 20,
+            11 * cols / 20
+        ])
+
+        self.hand_row_se = self.hand_row_nw + 10
+        self.hand_col_se = self.hand_col_nw + 10
+
+        size = self.hand_row_nw.size
+        for i in xrange(size):
+            cv2.rectangle(frame, (
+                self.hand_col_nw[i],
+                self.hand_row_nw[i]),
+                (self.hand_col_se[i], self.hand_row_se[i]),
+                (0, 255, 0),
+                1
+            )
+        black = np.zeros(frame.shape, dtype=frame.dtype)
+        frame_final = np.vstack([black, frame])
+        return frame
+
+    def train_hand(self, frame):
+        self.set_hand_hist(frame)
+        self.trained_hand = True
+
+    def set_hand_hist(self, frame):
+        # TODO use constants, only do HSV for ROI
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        roi = np.zeros([90, 10, 3], dtype=hsv.dtype)
+
+        size = self.hand_row_nw.size
+        for i in xrange(size):
+            roi[i * 10:i * 10 + 10, 0:10] = hsv[self.hand_row_nw[i]:self.hand_row_nw[i] + 10, self.hand_col_nw[i]:self.hand_col_nw[i] + 10]
+
+        self.hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+        cv2.normalize(self.hand_hist, self.hand_hist, 0, 255, cv2.NORM_MINMAX)
+
+
+def apply_hist_mask(frame, hist):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    cv2.filter2D(dst, -1, disc, dst)
+
+    ret, thresh = cv2.threshold(dst, 100, 255, 0)
+    thresh = cv2.merge((thresh, thresh, thresh))
+
+    cv2.GaussianBlur(dst, (3, 3), 0, dst)
+
+    res = cv2.bitwise_and(frame, thresh)
+    return res
+
+hand_detection = HandDetection()
 
 while(1):
-    rep, frame = cap.read()
-    fgmask = fgbg.apply(frame)
+    rep, frame = cam.read()
 
-    #frame = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY)
-    #fgmask = init_frame - frame
+    if not hand_detection.trained_hand:
+        frame = hand_detection.draw_hand_rect(frame)
 
-    blurred = cv2.GaussianBlur(fgmask, (45, 45), 0)
-    _, thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY_INV)
-    cv2.imshow('Thresholded', thresh)
-    #cv2.imshow('frame', fgmask)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+        if cv2.waitKey(1) == ord('h') & 0xFF:
+                hand_detection.train_hand(frame)
+    else:
+        frame = apply_hist_mask(frame, hand_detection.hand_hist)
 
-cap.release()
+    cv2.imshow('image', frame)
+
+cam.release()
 cv2.destroyAllWindows()
